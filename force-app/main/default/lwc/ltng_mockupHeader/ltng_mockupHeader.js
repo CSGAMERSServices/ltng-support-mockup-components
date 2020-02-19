29ef78fc9256b3d09deadf0e6d8606a8ffe7130e
Paul Roth <paulroth3d@gmail.com>
2020-02-19 17:14:20 -0600

    UPDATE: add in placeholders for the different components

---
+++ force-app/main/default/lwc/ltng_mockupHeader/ltng_mockupHeader.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Ltng_mockupHeader extends LightningElement {}
---
+++ force-app/main/default/lwc/ltng_mockupTable/ltng_mockupTable.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Ltng_mockupTable extends LightningElement {}
