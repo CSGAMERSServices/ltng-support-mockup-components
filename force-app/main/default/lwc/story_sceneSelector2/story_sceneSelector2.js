9855e187019bc2fd5cdedbab8058292ccccfa0b7
Paul Roth <paulroth3d@gmail.com>
2020-03-10 12:00:01 -0500

    CLEANUP: remove components that will not be used

--- force-app/main/default/lwc/story_sceneSelector2/story_sceneSelector2.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Story_sceneSelector2 extends LightningElement {}
-
-//-- check to see if I bind to a variable in the binding
-//-- do I need to then assign that to a property on the container
-//-- so I could then use that within the group
-
-//-- OR do I need to create a `storyReference` class
