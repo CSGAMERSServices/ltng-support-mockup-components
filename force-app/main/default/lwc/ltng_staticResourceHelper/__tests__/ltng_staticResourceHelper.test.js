c19f64de28ed9126819944f3bf16d357edf02aeb
Paul Roth <paulroth3d@gmail.com>
2020-03-11 19:37:04 -0500

    UPDATE: get static resource component started and querying

--- force-app/main/default/lwc/ltng_staticResourceHelper/__tests__/ltng_staticResourceHelper.test.js
+++ force-app/main/default/lwc/ltng_staticResourceHelper/__tests__/ltng_staticResourceHelper.test.js
@@ -1,9 +1,22 @@
 /* eslint-disable @lwc/lwc/no-inner-html */
 
+/* eslint-disable no-unused-vars */
 /** JEST Test for ltng_staticResourceHelper/__tests__/ltng_staticResourceHelper **/
 import { createElement } from 'lwc';
 import ltng_staticResourceHelper from 'c/ltng_staticResourceHelper';
+import { ENTER_KEY } from 'c/ltng_staticResourceHelper';
 // import { isArray } from 'util';
+
+import apexFindStaticResources from '@salesforce/apex/ltng_staticResourceHelperCtrl.findStaticResources';
+import { registerLdsTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+const apexFindStaticResourcesStub = registerLdsTestWireAdapter(apexFindStaticResources);
+
+const staticResourceResults = {
+  data: [{
+    type: 'StaticResource'
+  }]
+};
+/* eslint-enable no-unused-vars */
 
 const defaultProperties = {};
 
@@ -50,6 +63,9 @@ describe('c-ltng_staticResourceHelper', () => {
   it('can be created', () => {
     const ts = new TestSettings()
       .applyDefaultProperties()
+      .customSetup(customTS => {
+        customTS.element.exampleProp = '';
+      })
       .attachElement();
     
     expect(ts.element).not.toBe(null);
@@ -57,4 +73,51 @@ describe('c-ltng_staticResourceHelper', () => {
     // const div = ts.element.shadowRoot.querySelector('div');
     // expect(div.textContent).toBe('Hello, World!');
   });
+
+  describe('user presses return on the search', () => {
+    it('updates the query term', (done) => {
+      const ts = new TestSettings()
+        .applyDefaultProperties()
+        .attachElement();
+
+      const searchTerm = 'ltng_';
+
+      // const searchInputContainer = ts.element.shadowRoot
+      //   .querySelector('div.search-box');
+
+      const searchInput = ts.element.shadowRoot
+        .querySelector('div.search-box > lightning-input');
+      
+      expect(searchInput).toBeTruthy();
+
+      searchInput.value = searchTerm;
+      // see https://javascript.info/dispatch-events
+      const enterEvent = new CustomEvent('keyup', {
+        detail: {
+          keyCode: ENTER_KEY
+        },
+        keyCode: ENTER_KEY,
+        bubbles: true
+      });
+      searchInput.dispatchEvent(enterEvent);
+
+      expect(ts.element.queryTerm).toBe(searchTerm);
+
+      done();
+    });
+  });
+
+  /*
+  it('performs a search', (done) => {
+    const ts = new TestSettings()
+      .applyDefaultProperties()
+      .attachElement();
+
+    apexFindStaticResourcesStub.emit(staticResourceResults);
+
+    expect(ts.staticResources).toBeTruthy();
+
+    done();
+  });
+  */
 });
--- force-app/main/default/lwc/ltng_staticResourceHelper/ltng_staticResourceHelper.js
+++ force-app/main/default/lwc/ltng_staticResourceHelper/ltng_staticResourceHelper.js
@@ -1,4 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+
+import apexFindStaticResources from '@salesforce/apex/ltng_staticResourceHelperCtrl.findStaticResources';
+
+/**
+ * @typedef {Object} StaticResource
+ * @property {String} Id - id of the record
+ * @property {String} Name - name of the static resource
+ * @property {String} LastModifiedDate - date the resource was last modified
+ */
+
+const ENTER_KEY = 13;
 
 /**
  * This is a component to allow a user to upload static resources
@@ -7,4 +18,51 @@ import { LightningElement } from 'lwc';
  * 
  * This will hopefully make it much simpler to hold discussions with mockups.
  */
-export default class Ltng_staticResourceHelper extends LightningElement {}
+export default class Ltng_staticResourceHelper extends LightningElement {
+
+  /**
+   * The term to search for
+   * @type {String}
+   */
+  @api queryTerm = 'ltng_'
+
+  /**
+   * The static resources found
+   * @type {StaticResource[]}
+   */
+  @wire (apexFindStaticResources, { searchStr: '$queryTerm' })
+  handleResults({data, error}) {
+    console.log('results came in');
+    console.log(data);
+    console.log(error);
+    if (data) {
+      this.staticResources = data;
+    }
+  }
+
+  @api staticResources;
+
+  //-- handlers
+
+  /**
+   * Handles when the user presses the return key
+   * @param {CustomEvent} evt
+   */
+  handleKeyUp(evt) {
+    //-- @see https://developer.salesforce.com/docs/component-library/bundle/lightning-input/example
+    //-- specifically 'Search Input'
+    console.log('key up');
+    const isEnterKey = evt.keyCode === ENTER_KEY || evt.detail.keyCode === ENTER_KEY;
+    if (isEnterKey) {
+      this.queryTerm = this.searchQuery;
+    }
+  }
+
+  get searchQuery() {
+    return this.template.querySelector('div.search-box > lightning-input').value;
+  }
+}
+
+export {
+  ENTER_KEY
+};
